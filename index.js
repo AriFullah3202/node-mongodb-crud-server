46d5409a34023663ba48c5509b35a9b5ae3b0922
Arif <marifullaah@gmail.com>
2022-10-30 18:50:21 +0600

    inserted data into mongodb

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require("cors")
+const app = express();
 app.use(cors())
 app.use(express.json())
 
@@ -10,6 +11,31 @@ let users = [
     { id: 1, name: "arif", email: "arifullah@gmail.com" },
     { id: 2, name: "acib", email: "acib@gmail.com" }
 ]
+
+const uri = "mongodb+srv://userdb:S8nXMDUwnLg4b94y@cluster0.wg8wdsp.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        const database = client.db("simpleNode"); // this is database name simpleNode
+        const userCollection = database.collection("users"); // this is collection name like table
+        // create a document to insert
+
+        app.post('/users', async (req, res) => {
+            console.log(req.body)
+
+            const user = req.body;
+
+            console.log(user)
+            const result = await userCollection.insertOne(user);
+            console.log(`A document was inserted with the _id: ${result.insertedId}`);
+            res.send(user)
+        })
+
+
+    } finally {
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
@@ -32,16 +58,26 @@ app.get("/users", (req, res) => {
 })
 
 
-app.post('/users', (req, res) => {
-    console.log(req.body)
 
-    const user = req.body;
-    users.push(user);
-    res.send(user)
-    console.log(user)
-})
+// app.post('/users', (req, res) => {
+//     console.log(req.body)
+
+//     const user = req.body;
+//     users.push(user);
+//     res.send(user)
+//     console.log(user)
+// })
 
 app.listen(port, () => {
     console.log("server is running the port", port);
 })
 module.exports = app;
+
+// username : userdb
+// password : S8nXMDUwnLg4b94y
+
+
+
+
+
+
